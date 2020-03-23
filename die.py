cc4fb34ba77393380eedfd7bb9d3879003a537a8
didate <lyve.diallo@gmail.com>
2020-03-23 18:09:25 +0000

    Rolling Die D6

---
+++ die.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+class Die():
+    """A Class representing a single die."""
+
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll(self):
+        return randint(1, self.sides)
---
+++ die_visual.py
@@ -0,0 +1,24 @@
+from die import Die
+import pygal
+
+die = Die()
+
+results = []
+for roll_num in range(1000):
+    results.append(die.roll())
+
+# Analyze the results
+frequencies = []
+for value in range(1, die.sides + 1):
+    frequencies.append(results.count(value))
+
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Results"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual_svg')
